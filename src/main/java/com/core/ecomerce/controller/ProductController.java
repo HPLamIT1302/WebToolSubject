8f341b7b17aa46ce6d920f01a68037ffbdc27ea6
nam2002 <namdz9e@gmail.com>
2024-09-29 10:27:31 +0700

    fix update service (product controller, product-edit)

--- src/main/java/com/core/ecomerce/controller/ProductController.java
+++ src/main/java/com/core/ecomerce/controller/ProductController.java
@@ -7,9 +7,11 @@ import com.core.ecomerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
@@ -39,14 +41,18 @@ public class ProductController {
         model.addAttribute("product", new Product());
         List<Category> categories = categoryService.getAllCategories();
         model.addAttribute("categories", categories);
-        System.out.println("Categories loaded: " + categories);
         return "product-page/product-form";
     }
 
     @PostMapping
-    public String saveProduct(@ModelAttribute Product product) {
-        productService.saveProduct(product);
-        return "redirect:/products";
+    public String saveProduct(@ModelAttribute("product") Product product, Model model) {
+        try {
+            productService.saveProduct(product);
+            return "redirect:/products";
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error saving product");
+            return "product-page/product-form";
+        }
     }
 
     @GetMapping("/delete/{id}")
@@ -55,11 +61,28 @@ public class ProductController {
         return "redirect:/products";
     }
 
-    @PostMapping("/edit/{id}")
-    public String updateProduct(@PathVariable("id") int id, @ModelAttribute Product product) {
-        product.setId(id);
-        productService.updateProduct(product);
-        return "redirect:/products";
+    @GetMapping("/edit/{id}")
+    public String showEditProductPage(@PathVariable("id") int id, Model model) {
+        Product product = productService.getProductById(id);
+        Optional<Category> category = productService.getCategoryById(product.getCateId());
+        model.addAttribute("product", product);
+        model.addAttribute("categoryName", category.get().getName());
+        model.addAttribute("categories", productService.getAllCategories());
+        return "product-page/product-edit";
     }
+
+    @PostMapping("/{id}")
+    public String updateProduct(@PathVariable int id, @ModelAttribute("product") Product product, Model model) {
+        try {
+            product.setId(id);
+            productService.updateProduct(product);
+            return "redirect:/products";
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error updating product");
+            return "product-page/product-form";
+        }
+    }
+
+
 
 }
